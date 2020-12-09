2fba997af914c73742e54f6ef97dd10e0ea0a992
Allan Menoret <allan.menoret@gmail.com>
2020-12-10 00:41:38 +0100

    clean up on the day 06

--- src/Core/Day06.fs
+++ src/Core/Day06.fs
@@ -44,3 +44,21 @@ module FireHazard =
         let board = createBoard
         applyInst instructions board |> Map.toSeq |> Seq.filter (fun (_, state) -> state = On) |> Seq.length
 
+    let generatePos {X=x1;Y=y1} {X=x2;Y=y2} value =
+        seq {x1..x2} |> Seq.allPairs (seq {y1..y2})
+            |> Seq.map (fun (x,y) -> ({X=x;Y=y}, value))
+
+    let rec generateBoard instructions =
+        match instructions with
+        | [] -> Seq.empty
+        | (TurnOn (pos1, pos2))::rest -> seq {yield! generatePos pos1 pos2 1; yield! generateBoard rest }
+        | (TurnOff (pos1, pos2))::rest -> seq {yield! generatePos pos1 pos2 -1; yield! generateBoard rest }
+        | (Toogle (pos1, pos2))::rest -> seq {yield! generatePos pos1 pos2 2; yield! generateBoard rest }
+
+    let calcList list =
+        list |> Seq.fold (fun acc (_, n) -> if n = -1 && acc=0 then 0 else acc+n) 0
+
+    let howManyOn2 (instructions: Instruction list) =
+        let board = generateBoard instructions
+        board |> Seq.groupBy (fun (pos, _) -> pos) |> Seq.sumBy (fun (_, list) -> calcList list)
+
--- tests/Core.Tests/Day06Tests.fs
+++ tests/Core.Tests/Day06Tests.fs
@@ -7,13 +7,24 @@ open Day06Inputs
 open Day06Core.FireHazard
 open Day06Parser.FireHazardParser
 
-[<Fact>]
-let ``small set part 01`` () =
-    smallSet |> scan |> howManyOn |> should equal 998996
-
 // turn off because super slow copy the map all the time <3
+
+// [<Fact>]
+// let ``small set part 01`` () =
+//     smallSet |> scan |> howManyOn |> should equal 998996
+
 // [<Fact>]
 // let ``big set part 01`` () =
 //     bigSet |> scan |> howManyOn |> should equal 569999
+
+
+
+// [<Fact>]
+// let ``small set part 02`` () =
+//     smallSet |> scan |> howManyOn2 |> should equal 1001996
+
+// [<Fact>]
+// let ``big set part 02`` () =
+//     bigSet |> scan |> howManyOn2 |> should equal 17836115
 
 
